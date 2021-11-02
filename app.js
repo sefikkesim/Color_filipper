ee77465a64831c243110c435fde15217021cc722
sefikkesim <sefikkesim@gmail.com>
2021-11-02 19:59:32 +0200

    .

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const color1 =["green", "red", "rgba(133,122,200)",
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color")
+const nav = document.querySelector(".nav-center")
+const container = document.querySelector(".container")
 
 btn.addEventListener("click",()=> {
     document.body.style.backgroundColor = rColor();
-    color.textContent = rColor()
-
+    nav.style.backgroundColor = rColor();
+    container.style.backgroundColor  = rColor();
+    btn.style.backgroundColor = rColor();
 })
 
 let number = () => Math.floor(Math.random() * 256)
